d5ae97acfe99a100b8ceff12f3beb89f9fc28bb1
Wilsher <UKX3646@NissanEU.NMCorp.Nissan.Biz>
2018-07-17 14:30:40 +0100

    carosuel

--- DoukiView/src/app/featured-films/featured-films.component.ts
+++ DoukiView/src/app/featured-films/featured-films.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedFilmsComponent implements OnInit {
 
-  constructor() { }
+  imageArray: String[];
+  imageIndex: number;
+  
+
+
+  constructor() {
+    this.imageIndex = 0;
+    this.imageArray = ["https://resizing.flixster.com/EEy-z7iTK_ed7y78dbkdwyVKBWs=/4096x1862/v1.bjsxOTYyMjA1O2o7MTc3OTc7MTIwMDs0MDk2OzE4NjI", "https://cdn.movieweb.com/img.news.tops/NEFa39xFnSMrIM_1_b/Jurassic-World-3-Story-Fallen-Kingdom-Connection.jpg", "https://i.kym-cdn.com/photos/images/original/000/964/872/5b5.jpg"];
+    
+   }
 
   ngOnInit() {
   }
 
+  //Shift the carousel right if possible, wrap around if not
+  moveRight() {
+   
+    if (this.imageIndex < this.imageArray.length - 1) {
+      this.imageIndex++;
+     
+    } else {
+      this.imageIndex = 0;
+     
+    }
+
+  }
+  //Shif the carousel left if possible, wrap around if not
+  moveLeft() {
+    if (this.imageIndex > 0) {
+      this.imageIndex--;
+    } else {
+      this.imageIndex = this.imageArray.length - 1;
+    }
+  }
+
 }
+
+
